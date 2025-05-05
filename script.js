6d3a43a1af5ed0387015a87f4371de1b508a6367
Shariq Hashmi <127607558+bonkers23@users.noreply.github.com>
2025-05-05 22:35:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+let inputs = document.querySelectorAll(".code");
+
+    inputs[0].focus();
+
+    for (let i = 0; i < inputs.length; i++) {
+      inputs[i].addEventListener("input", () => {
+        if (inputs[i].value.length === 1 && i < inputs.length - 1) {
+          inputs[i + 1].focus();
+        }
+      });
+      inputs[i].addEventListener("keydown", (e) => {
+        if (e.key === "Backspace" && inputs[i].value === "" && i > 0) {
+          inputs[i - 1].value = ""; 
+          inputs[i - 1].focus();
+        }
+      });
+    }
